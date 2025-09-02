c388de8eb4fd08a83a54e4f4ff436852ef5372c1
NeckitWin <neckit.v@gmail.com>
2025-09-02 22:33:02 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button, Flex, Text } from "@radix-ui/themes"
+
+const App = () => {
+  return (
+    <>
+    <Flex direction="column" gap="2" >
+      <Text>Test</Text>
+      <Button>Click</Button>
+
+    </Flex>
+    </>
+  )
+}
+
+export default App
