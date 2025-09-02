dfcffc0b148cbb740386d768b6bf62d018b21b19
NeckitWin <neckit.v@gmail.com>
2025-09-02 23:03:06 +0200

    feat: simple UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Button, Flex, Text } from "@radix-ui/themes"
+import { Box, Button, Flex, Text } from "@radix-ui/themes"
 
 const App = () => {
   return (
-    <>
-    <Flex direction="column" gap="2" >
-      <Text>Test</Text>
-      <Button>Click</Button>
+    <Flex direction="row" gap="4" p="4" >
+      
+      <Flex direction="column" gap="2" width='50%' >
+        <Button variant="soft" color="cyan">Click 1</Button>
+        <Button variant="soft" color="crimson">Click 2</Button>
+      </Flex>
+      <Box  width='50%'>
+        <Text>Box Content</Text>
+      </Box>
 
-    </Flex>
-    </>
+    </Flex >
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import "@radix-ui/themes/styles.css";
 import { Theme } from '@radix-ui/themes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Theme  grayColor='sand' radius='large' scaling='95%' >
+    <Theme appearance='dark' >
       <App />
     </Theme>
   </StrictMode>,
