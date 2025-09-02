5b049fb9fbfc914e62edc01cb27e60ae573a9847
NeckitWin <neckit.v@gmail.com>
2025-09-02 23:17:40 +0200

    chore: add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { Box, Button, Flex, Text } from "@radix-ui/themes"
+import { Box, Button, Flex } from "@radix-ui/themes"
+import { Canvas } from "@react-three/fiber"
 
 const App = () => {
   return (
     <Flex direction="row" gap="4" p="4" >
       
-      <Flex direction="column" gap="2" width='50%' >
+      <Flex direction="column" gap="2" width='20%' >
         <Button variant="soft" color="cyan">Click 1</Button>
         <Button variant="soft" color="crimson">Click 2</Button>
       </Flex>
-      <Box  width='50%'>
-        <Text>Box Content</Text>
+      <Box width='80%' className="bg-white rounded-lg h-full">
+        <Canvas>
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
       </Box>
 
     </Flex >
